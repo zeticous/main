50fb4fd75294dc20e26adb6722d7d00ed1f9182d
Kenny\imissso <e0004448@u.nus.edu>
2017-03-16 17:43:11 +0800

    hided

--- src/main/java/seedu/address/model/person/TaskUtil.java
+++ src/main/java/seedu/address/model/person/TaskUtil.java
@@ -5,15 +5,23 @@ public class TaskUtil {
     public static final String DUMMY_START_DATE = "1/1/1900 00:00";
     public static final String DUMMY_END_DATE = "1/1/2100 00:00";
 	
+    private static boolean hasStart(ReadOnlyPerson task) {
+    	return task.getStartDate().toString() != DUMMY_START_DATE;
+    }
+    
+    private static boolean hasEnd(ReadOnlyPerson task) {
+    	return task.getEndDate().toString() != DUMMY_END_DATE;
+    }
+    
     public static boolean isFloating(ReadOnlyPerson task){
-        return (task.getEndDate()==null);
+        return (hasStart(task) && hasEnd(task));
     }
     
     public static boolean isDeadline(ReadOnlyPerson task){
-        return (task.getEndDate()!=null && task.getStartDate()==null );
+        return (hasStart(task) && !hasEnd(task) || !hasStart(task) && hasEnd(task));
     }
     
     public static boolean isEvent(ReadOnlyPerson task){
-        return (task.getEndDate()!=null && task.getStartDate()!=null);
+        return (!hasStart(task) && !hasEnd(task));
     }
 }
