b604ad604b752c0b2dd4b34c02acefba2456b118
Kenny\imissso <e0004448@u.nus.edu>
2017-03-16 16:49:41 +0800

    added dummy

--- src/main/java/seedu/address/model/person/TaskUtil.java
+++ src/main/java/seedu/address/model/person/TaskUtil.java
@@ -1,6 +1,10 @@
 package seedu.address.model.person;
 
 public class TaskUtil {
+
+    public static final String DUMMY_START_DATE = "1/1/1900 00:00";
+    public static final String DUMMY_END_DATE = "1/1/2100 00:00";
+	
     public static boolean isFloating(ReadOnlyPerson task){
         return (task.getEndDate()==null);
     }
