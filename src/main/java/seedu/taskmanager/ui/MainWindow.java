7b2b982799bacc18b182c0abf33e7839d3834200
zeticous <e0004327@u.nus.edu>
2017-03-22 15:44:24 +0800

    Fixed nullpointerexception in app icon

--- src/main/java/seedu/taskmanager/ui/MainWindow.java
+++ src/main/java/seedu/taskmanager/ui/MainWindow.java
@@ -24,7 +24,7 @@ import seedu.taskmanager.model.task.ReadOnlyTask;
  */
 public class MainWindow extends UiPart<Region> {
 
-    private static final String ICON = "/images/address_book_32.png";
+    private static final String ICON = "/images/task_manager_32.png";
     private static final String FXML = "MainWindow.fxml";
     private static final int MIN_HEIGHT = 600;
     private static final int MIN_WIDTH = 450;
