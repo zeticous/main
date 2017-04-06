96d4a4aef8fd05b3e393a98eaf0ec1b77aba7a43
Liu Peng <liu_peng@u.nus.edu>
2017-04-06 21:27:42 +0800

    new test case2

---
+++ src/test/java/seedu/taskmanager/model/task/TaskDateTest.java
@@ -0,0 +1,14 @@
+package seedu.taskmanager.model.task;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskDateTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
