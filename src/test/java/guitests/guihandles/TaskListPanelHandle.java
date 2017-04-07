a82efaeec6276385000d303135bc1d31ca2f7e13
Liu Peng <liu_peng@u.nus.edu>
2017-04-07 16:32:35 +0800

    checkstyle fix

--- src/test/java/guitests/guihandles/TaskListPanelHandle.java
+++ src/test/java/guitests/guihandles/TaskListPanelHandle.java
@@ -104,7 +104,7 @@ public class TaskListPanelHandle extends GuiHandle {
     }
 
     public TaskCardHandle navigateToTask(String name) {
-        guiRobot.sleep(1000); // Allow a bit of time for the list to be updated
+        guiRobot.sleep(300); // Allow a bit of time for the list to be updated
         final Optional<ReadOnlyTask> task =
                 getListView().getItems().stream().filter(p -> p.getName().fullName.equals(name)).findAny();
         if (!task.isPresent()) {
